0769ae97370b64211577039a1b3525bc908cd120
Blanvi Barbosa <blanvisb@gmail.com>
2018-02-15 03:32:24 -0200

    first commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ProjectLaravel\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ProjectLaravel\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
