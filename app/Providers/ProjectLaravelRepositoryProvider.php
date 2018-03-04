c1d5813368cae86dfe8cf0eebf685971908d6120
Blanvi Barbosa <blanvisb@gmail.com>
2018-03-04 04:26:47 -0300

    alteracoes repository

---
+++ app/Providers/ProjectLaravelRepositoryProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProjectLaravel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProjectLaravelRepositoryProvider extends ServiceProvider {
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->app->bind(
+			\ProjectLaravel\Repositories\ClientRepository::class ,
+			\ProjectLaravel\Repositories\ClientRepositoryEloquent::class
+		);
+	}
+}
---
+++ app/Repositories/ClientRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProjectLaravel\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+interface ClientRepository extends RepositoryInterface {
+
+}
