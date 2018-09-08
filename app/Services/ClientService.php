29d7322141a9eaf8253e49406fd84ebef46bb2fa
Blanvi Barbosa <blanvisb@gmail.com>
2018-09-07 21:23:35 -0300

    atualizacao Services/ClientService

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -1,23 +1,60 @@
 <?php
 
 namespace ProjectLaravel\Services;
+namespace ProjectLaravel\Validators\ClientValidator;
 
 class ClientService {
 
+	/**
+	 * @var ClientRepository
+	 */
 	protected $repository;
 
-	public function __construct(clientRepository $repository) {
+	/**
+	 * @var ClientValidator
+	 */
+	protected $validator;
+
+	public function __construct(ClientRepository $repository, ClientValidator $validator) {
+
 		$this->repository = $repository;
+		$this->validator  = $validator;
 	}
 
 	public function create(array $data) {
+
+		try {
+
+			$this->validator->with($data)->passesOrFail();
+
+			return $this->repository->create($data);
+
+		} catch (ValidatorException $e) {
+
+			return [
+				'error'   => true,
+				'message' => $e->getMessageBag()
+			];
+		}
 		//enviar um email
 		//disparar notificação
 		//postar um tweet
-		return $this->repository->create($data);
 	}
 
 	public function update(array $data, $id) {
-		return $this->repository->update($data, $id);
+
+		try {
+
+			$this->validator->with($data)->passesOrFail();
+
+			return $this->repository->update($data, $id);
+
+		} catch (ValidatorException $e) {
+
+			return [
+				'error'   => true,
+				'message' => $e->getMessageBag()
+			];
+		}
 	}
 }
